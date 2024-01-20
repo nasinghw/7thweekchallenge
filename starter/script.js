78fbf0fc43348668411c71eca5c8623de9d648e6
nasinghw <151625048+nasinghw@users.noreply.github.com>
2024-01-20 16:44:44 +0000

    Calendar data

--- starter/script.js
+++ starter/script.js
@@ -1,8 +1,10 @@
   $(document).ready(function () {
     // Display the current date
+    
     $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY [at] hh:mm:ss a"));
   
     // Calendar timeblocks to display
+
     var container = $(".container");
     for (var hour = 9; hour <= 17; hour++) {
       var timeblock = $("<div>").addClass("row time-block");
@@ -11,6 +13,7 @@
       var saveBtn = $("<button>").addClass("col-1 saveBtn").html('<i class="fas fa-save"></i>');
   
       // Add hour-dependent classes (past, present, future)
+
       if (hour < dayjs().hour()) {
         timeblock.addClass("past");
       } else if (hour === dayjs().hour()) {
@@ -22,5 +25,26 @@
       timeblock.append(hourCol, textArea, saveBtn);
       container.append(timeblock);
     }
-      
+     
+    // Save calendar events and save them in local storage
+
+    $(".saveBtn").on("click", function () {
+        // Retrieve the text and corresponding hour
+
+        var text = $(this).siblings(".description").val();
+        var hour = parseInt($(this).siblings(".hour").text());
+    
+        // Save to local storage
+
+        localStorage.setItem("event_" + hour, text);
+      });
+    
+      // Load events from local storage
+
+      for (var hour = 9; hour <= 17; hour++) {
+        var text = localStorage.getItem("event_" + hour);
+        if (text !== null) {
+          $(".hour:contains(" + hour + ":00)").siblings(".description").val(text);
+        }
+      }
   });
