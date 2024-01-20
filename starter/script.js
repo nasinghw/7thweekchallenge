06d5e341dcf4d245d7379867715743d6ee731647
nasinghw <151625048+nasinghw@users.noreply.github.com>
2024-01-20 16:31:02 +0000

    Current date and time

---
+++ starter/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    // Display the current date
+    $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY [at] hh:mm:ss a "));
+  
+    // Add other functionality here...
+  });
+
+  
